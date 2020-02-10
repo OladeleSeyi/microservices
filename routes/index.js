9b8928f99f4e0d65e31ce9047f8cd2385ca6bc39
Oladele Seyi <toothy61@gmail.com>
2020-02-10 18:50:48 +0100

    setup base controller and routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 import { Router } from "express";
 import { NotFoundError, APIError } from "../utils/errors";
-import routes from "./routes";
+import api from "./routes";
 
 const router = Router();
-router.get("/", (req, res) => {
+router.all("/", (req, res) => {
   return res.json({ message: "Welcome to the Api. Choose a route" });
+});
+router.use("/api", api);
+
+router.use((req, res) => {
+  throw new NotFoundError(
+    `${req.method.toUpperCase()} Route ${req.url} Not Found!`
+  );
+});
+//Error handler
+
+router.use((err, req, res, next) => {
+  const response = {
+    message: err.message
+  };
+  console.log(process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "development" && !(err instanceof APIError)) {
+    // Unknown server error. Response with stack trace for easier debugging
+    response.stack = err.stack;
+  }
+  return res.status(err.status || 500).json(response);
 });
 
 export default router;
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 
 import Submissions from "../controllers/submissions";
+import submissions from "../controllers/submissions";
 
 const router = Router();
+
+router.post("/submissions", Submissions.addSubmission);
+router.get("./submissions", Submissions.getAllSubmissions);
+router.get("/submissions/:id", Submissions.getSubmission);
 
 export default router;
