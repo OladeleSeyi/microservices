b7ba14c86f8585f9a987e6fb9c4ffd5f0b2f1dd3
Oladele Seyi <toothy61@gmail.com>
2020-02-13 20:49:25 +0100

    fix variables in Nedb.js file

--- models/Nedb.js
+++ models/Nedb.js
@@ -1,10 +1,10 @@
 import Datastore from "nedb";
-import bcryptjs from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const db = new Datastore({ filename: process.env.NEDB, autoload: true });
 const db2 = new Datastore({ filename: process.env.NEDB2, autoload: true });
 
-let pass = bcryptjs.hashSync(process.env.ADMIN_PASS, 10);
+let pass = bcrypt.hashSync(process.env.ADMIN_PASS, 10);
 
 let admin = {
   id: "lala",
